df7f7d14fcf085769514ec2de36c70e2d2d4ee73
Adrien Turmo <adrien.turmo@gmail.com>
2017-07-26 23:22:41 +0200

    simplify Transactions class

--- src/main/java/Transactions.java
+++ src/main/java/Transactions.java
@@ -4,50 +4,43 @@ import java.util.stream.Collectors;
 
 
 public class Transactions {
-    private List<OperationType> operationsType;
-    private List<Double> operations;
-    private List<String> transactionsDate;
     private DateProvider dateProvider;
 
     private List<Transaction> transactions;
 
     public Transactions(DateProvider dateProvider) {
         this.dateProvider = dateProvider;
-        operationsType = new ArrayList();
-        operations = new ArrayList();
-        transactionsDate = new ArrayList();
 
         transactions = new ArrayList();
     }
 
 
     public void addDeposit(double amount) {
-        operationsType.add(OperationType.DEPOSIT);
-        operations.add(amount);
-        transactionsDate.add(dateProvider.todaysDateAsString());
 
         Transaction transaction = new Transaction(amount,OperationType.DEPOSIT,dateProvider.todaysDateAsString());
         transactions.add(transaction);
     }
 
     public void addWithdrawal(double amount) {
-        operationsType.add(OperationType.WITHDRAWAL);
-        operations.add(amount);
-        transactionsDate.add(dateProvider.todaysDateAsString());
-
         Transaction transaction = new Transaction(amount,OperationType.WITHDRAWAL,dateProvider.todaysDateAsString());
         transactions.add(transaction);
     }
 
     public List<OperationType> getOperationsType() {
-        return transactions.stream().map(t -> t.getOperationType()).collect(Collectors.toList());
+        return transactions.stream()
+                .map(t -> t.getOperationType())
+                .collect(Collectors.toList());
     }
 
     public List<Double> getOperations() {
-        return operations;
+        return transactions.stream()
+                .map(t -> t.getAmount())
+                .collect(Collectors.toList());
     }
 
     public List<String> getTransactionsDate() {
-        return transactionsDate;
+        return transactions.stream()
+                .map(t -> t.getDate())
+                .collect(Collectors.toList());
     }
 }
